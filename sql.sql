7a406f7371ef212043895867348a08c147b3f681
Jesus Rumie Vittar <23528273+jrumievittar@users.noreply.github.com>
2023-09-15 17:12:35 +0200

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,9 @@
+select count(*) from 'estudiantes' where 'edad' > 30;
+
+select * from 'estudiantes' where 'color' is not 'yellow';
+
+select * from 'estudiantes' where 'edad' between 50 and 60;
+
+drop 'estudiantes';
+
+select * from 'estudiantes' where 'any' like '2023-01-10';
